5474790478e2cd598722149b7c8e037ab8db25dc
Juanedu005 <je.ballesteros@uniandes.edu.co>
2025-05-18 18:24:48 -0500

    commit jUnit

---
+++ src/test/UsuarioTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class UsuarioTest {
+
+}
